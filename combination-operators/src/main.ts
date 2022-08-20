289e9426a0b5e150e98ae7788e864e1776daefee
Bogdan Liusik <bodialiusik@gmail.com>
2022-08-20 20:39:52 +0300

    combineLatest, concat

--- combination-operators/src/main.ts
+++ combination-operators/src/main.ts
@@ -1,11 +1,19 @@
-import { combineLatestAll } from 'rxjs';
+import { combineLatestAll, fromEvent, map, scan, startWith, combineLatest, Observable, delay, EMPTY, concat } from 'rxjs';
 import { combineLatestAllSource$ } from './combine-latest-all';
 
 const combiteLastestAllButtonId = 'combine-latest-all';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
+    <div><p id="wait-message"></p></div>
     <button id=${ combiteLastestAllButtonId }>Combine lastest all</button>
+    <div>
+      <button id="red">Red</button>
+      <button id="black">Black</button>
+    </div>
+    <div>Red: <span id="red-total"></span></div>
+    <div>Black: <span id="black-total"></span></div>
+    <div>Total: <span id="total"></span></div>
   </div>
 `;
 
@@ -32,3 +40,35 @@ document.getElementById(combiteLastestAllButtonId)?.addEventListener('click', ()
   });
 });
 
+const redTotal = document.getElementById('red-total');
+const blackTotal = document.getElementById('black-total');
+const total = document.getElementById('total');
+
+const addOneClick$ = (id: string): Observable<number> => {
+  return fromEvent(document.getElementById(id)!, 'click').pipe(
+    map(() => 1),
+    scan((acc, curr) => acc + curr, 0),
+    startWith(0)
+  );
+};
+
+combineLatest([addOneClick$('red'), addOneClick$('black')]).subscribe(
+  ([red, black]: any) => {
+    redTotal!.innerHTML = red;
+    blackTotal!.innerHTML = black;
+    total!.innerHTML = red + black;
+  }
+);
+
+const waitMessage = document.getElementById('wait-message')!;
+const delayedMessage = (message: string | number, delayedTime: number = 1000): Observable<string | number> => {
+  return EMPTY.pipe(startWith(message), delay(delayedTime));
+};
+
+concat(
+  delayedMessage('Get Ready!', 500),
+  delayedMessage(3),
+  delayedMessage(2),
+  delayedMessage(1),
+  delayedMessage('Go!')
+).subscribe((message: any) => (waitMessage.innerHTML = message));
